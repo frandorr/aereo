f45304d18f31344aff7b34c7b2d43f32042fbad8
Francisco Dorr <fran.dorr@gmail.com>
2026-03-03 19:57:41 +0000

    feat[spatial]: add grid and spatial extent component
    Add core classes for representing spatial extents and processing geometry.
    Includes `GridSpatialExtent`, `GridDefinition`, and integration with geopandas
    and rasterio to resolve geospatial overlap lookups. Add new unit tests for
    grid behavior.
    
    Closes previous task "Creating Spatial Component"

--- components/aer/spatial/__init__.py
+++ components/aer/spatial/__init__.py
@@ -3,15 +3,21 @@ Spatial component for storing geometry features, AOI types, and other spatial ab
 """
 
 from aer.spatial.core import (
+    Grid,
     GridCell,
     GridDefinition,
     GridSpatialExtent,
     reproject_polygon,
+    get_utm_epsg_from_geometry,
+    get_utm_zone_from_latlng,
 )
 
 __all__ = [
+    "Grid",
     "GridCell",
     "GridDefinition",
     "GridSpatialExtent",
     "reproject_polygon",
+    "get_utm_epsg_from_geometry",
+    "get_utm_zone_from_latlng",
 ]
