87ad76cdacbff42e121757ed207b4022815e5474
Francisco Dorr <fran.dorr@gmail.com>
2026-03-07 19:02:26 +0000

    test: remove obsolete earthaccess plugin registration and schema tests

--- test/integration/test_plugins.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from aer.plugin import plugin_registry
-
-
-@pytest.mark.integration
-def test_earthaccess_registered():
-    """Verify that the earthaccess plugin is correctly registered."""
-    names = {m.name for m in plugin_registry.all()}
-    assert "earthaccess" in names
