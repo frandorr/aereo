056d99d60501c51dc2836ad9180658d3194f3ab7
Francisco Dorr <fran.dorr@gmail.com>
2026-03-09 20:45:46 +0000

    refactor(spectral): use structlog and cleanup legacy fetchers
    - Refactored spectral/config.py to use structlog for consistency.
    - Removed legacy goes_fetcher and search_goes_aws components and tests.
    - Updated pyproject.toml to remove deleted bricks.

--- components/aer/goes_fetcher/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Fetch GOES assets and S3 bucket hrefs for products and bands
-"""
-
-from aer.goes_fetcher.core import GOESSearchQuery
-
-__all__ = ["GOESSearchQuery"]
--- components/aer/search_goes_aws/__init__.py
+++
@@ -1,3 +0,0 @@
-from aer.search_goes_aws.core import search_goes_aws
-
-__all__ = ["search_goes_aws"]
