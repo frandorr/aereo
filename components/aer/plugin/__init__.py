15de3e7a4fcee197e181896bbdb3f7ea512712d5
Francisco Dorr <fran.dorr@gmail.com>
2026-03-19 20:27:50 +0000

    feat(plugin): export run_search and run_extract from __init__

--- components/aer/plugin/__init__.py
+++ components/aer/plugin/__init__.py
@@ -4,6 +4,16 @@ from aer.plugin.core import (
     plugin_registry,
     Pipeline,
     PluginInfo,
+    run_search,
+    run_extract,
 )
 
-__all__ = ["PluginRegistry", "plugin", "plugin_registry", "Pipeline", "PluginInfo"]
+__all__ = [
+    "PluginRegistry",
+    "plugin",
+    "plugin_registry",
+    "Pipeline",
+    "PluginInfo",
+    "run_search",
+    "run_extract",
+]
