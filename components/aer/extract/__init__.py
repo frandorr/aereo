e1bccd7a166631174b854e68e1e0a5a2b8280919
Fran Dorr <fran.dorr@gmail.com>
2026-03-25 19:51:32 +0000

    chore: export ExtractionTask and cleanup dependency bounds

--- components/aer/extract/__init__.py
+++ components/aer/extract/__init__.py
@@ -1,3 +1,3 @@
-from .core import ExtractedResultSchema, ExtractPlugin
+from .core import ExtractedResultSchema, ExtractPlugin, ExtractionTask
 
-__all__ = ["ExtractedResultSchema", "ExtractPlugin"]
+__all__ = ["ExtractedResultSchema", "ExtractPlugin", "ExtractionTask"]
