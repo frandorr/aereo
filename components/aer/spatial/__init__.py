22ded08ad2d29c3fc4af5d48ad633eb458fd3875
Fran Dorr <fran.dorr@gmail.com>
2026-04-16 14:20:02 +0000

    refactor: remove obsolete spatial/temporal components and recreate spatial tests

--- components/aer/spatial/__init__.py
+++ components/aer/spatial/__init__.py
@@ -2,15 +2,6 @@
 Spatial component for storing geometry features, AOI types, and other spatial abstractions.
 """
 
-from aer.spatial.core import (
-    GridCell,
-    GridCellOri,
-    GridDefinition,
-    OverlapMode,
-    add_overlapping_cells,
-    find_overlapping_cells,
-)
-from aer.spatial.majortom import Grid
 from aer.spatial.utils import (
     get_utm_epsg_from_geometry,
     get_utm_zone_from_latlng,
@@ -18,14 +9,7 @@ from aer.spatial.utils import (
 )
 
 __all__ = [
-    "GridDefinition",
-    "GridCellOri",
-    "GridCell",
-    "OverlapMode",
-    "Grid",
-    "add_overlapping_cells",
     "get_utm_epsg_from_geometry",
     "get_utm_zone_from_latlng",
     "reproject_geom",
-    "find_overlapping_cells",
 ]
